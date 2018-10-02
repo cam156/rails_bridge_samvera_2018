1290755f575343c9a81923e0e0734b6bdc6b9b77
Carolyn Cole <cam156@psu.edu>
2018-10-02 15:16:44 -0400

    Adding view and view tests

--- testapp/spec/views/oranges/show.html.erb_spec.rb
+++ testapp/spec/views/oranges/show.html.erb_spec.rb
@@ -1,7 +1,15 @@
 require "rails_helper"
 
 describe "oranges/show.html.erb" do
+  let(:tree)   { Tree.new(id: 'tree_id') }
+  let(:orange) { Orange.new(name: 'Navel',  tree: tree) }
+
   it "displays the orange" do
-    skip "write your test here"
+    assign(:orange, orange)
+
+    render
+
+    expect(rendered).to include "Orange title: #{orange.name}"
+    expect(rendered).to include "Orange tree id: #{tree.id.to_s}"
   end
 end
